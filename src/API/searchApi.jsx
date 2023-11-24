2b5b55a9a98d6f2fd07866ed95d00c82c29f3c54
“nimoseel” <fromsomini@gmail.com>
2023-11-24 20:03:06 +0900

    Feature : 검색결과페이지 페이지네이션 기능 추가

--- src/API/searchApi.jsx
+++ src/API/searchApi.jsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import { API_URL } from '../constants/API_URL';
 
-export const getSearchData = async(search) => {
+export const getSearchData = async(activePage, searchValue) => {
     try{
-        const res = await axios.get(API_URL + `/products/?search=${search}`)
+        const res = await axios.get(API_URL + `/products/?page=${activePage}&search=${searchValue}`)
         return res;
     }catch(error){
         return error;
--- src/pages/SearchResultPage/SearchResult.jsx
+++ src/pages/SearchResultPage/SearchResult.jsx
@@ -1,23 +1,58 @@
-import React from "react";
-import { Link } from 'react-router-dom';
+import React,{useState, useEffect} from "react";
+import { Link, useNavigate } from 'react-router-dom';
 import { useLocation } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import ProductCard from '../../components/ProductCard/ProductCard';
 import * as S from '../SearchResultPage/_style'
+import { getSearchData } from "../../API/searchApi";
+import Paging from '../../components/Paging/Paging';
+import Loading from '../../components/Loading/Loading';
+
 
 
 const SearchResult = () => {
-    const location = useLocation();
-    const searchData = location.state.searchData;
-    const searchValue = location.state.searchWord;
-    const searchResults = searchData.results;
-    console.log(searchResults);
+    const navigate = useNavigate();
+    const location = useLocation(); 
+
+    const queryParams = new URLSearchParams(location.search);
+    const searchValue = queryParams.get('search');
+    const page = queryParams.get('page');
+
+    const [ searchData, setSearchData ] = useState({});
+    const [ searchResults, setSearchResults ] = useState([]);
+    const [ activePage, setActivePage ] = useState(parseInt(page) || 1);
+    const [ loading, setLoading ] = useState(false);
+
+    useEffect(()=>{
+        const fetchData = async () => {
+            setLoading(true);
+
+            if (searchValue) {
+                try {
+                    getSearchData(activePage, searchValue).then(res => {
+                        setSearchData(res.data);
+                        setSearchResults(res.data.results);
+                        setLoading(false);
+                    });
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+        };
+        fetchData();
+    },[searchValue, activePage]);
+
+    const handlePageChange = (e) => {
+        setActivePage(e);
+        navigate(`/search?page=${e}&search=${searchValue}`);
+    };
 
     return (
         <>
             <Header/>
             <S.Main>
+            {loading && <Loading/>}
                 <S.SearchCount>
                     <S.SearchTxt>
                         {searchValue}
@@ -30,7 +65,7 @@ const SearchResult = () => {
                         }
                     </S.SearchResultTxt>
                 </S.SearchCount>
-                { !searchData.count ? 
+                { searchData.count === 0 ? 
                     <S.NoResult/>
                     :
                     <S.ProductUl>
@@ -48,6 +83,14 @@ const SearchResult = () => {
                     </S.ProductUl>
                 }
             </S.Main>
+            {searchData && searchData.count > 15 &&
+                <Paging
+                    page={activePage}
+                    count={searchData.count}
+                    setPage={handlePageChange}
+                />
+            }
+
             <Footer/>
         </>
     )
