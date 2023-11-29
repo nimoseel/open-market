60d532fb168eb8ddc018264472b2b877fa563023
“nimoseel” <fromsomini@gmail.com>
2023-11-29 23:17:48 +0900

    Rename : SellerHeader -> SellerCenterHeader로 파일명 변경

--- src/components/Header/SellerCenterHeader.jsx
+++ src/components/Header/SellerCenterHeader.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import * as S from '../Header/_style';
+import * as S from './_style';
 
 const SellerHeader = () => {
     return (
-        <S.SellerHeaderDiv>
+        <S.HeaderDiv>
             <S.SellerHeaderContent>
                 <Link to={'/'}>
                     <S.LogoIcon alt='로고 이미지' />
                 </Link>
                 <S.SellerHeaderTitle>판매자 센터</S.SellerHeaderTitle>
             </S.SellerHeaderContent>
-        </S.SellerHeaderDiv>
+        </S.HeaderDiv>
     )
 };
 
