ffdb32df24d36a5f2de82f33783a1d5e7eaa3678
“nimoseel” <fromsomini@gmail.com>
2023-11-29 23:39:27 +0900

    Style : 작은따옴표로 변경

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import Loader from "react-spinners/RotateLoader";
+import React from 'react';
+import Loader from 'react-spinners/RotateLoader';
 import * as S from '../Loading/_style';
 
 function Loading() {
@@ -9,7 +9,7 @@ function Loading() {
                 <Loader
                     size={12}
                     margin={15}
-                    color="var(--main)"
+                    color='var(--main)'
                     />
             </S.LoadingDiv>
         </S.LoadingBg>
