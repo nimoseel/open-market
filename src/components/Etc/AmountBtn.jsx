0002cc4a18378497b675a126551bb8087bad0855
“nimoseel” <fromsomini@gmail.com>
2023-11-29 23:30:44 +0900

    Fix : isOne 속성 추가

--- src/components/Etc/AmountBtn.jsx
+++ src/components/Etc/AmountBtn.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import minusIcon from '../../assets/icon-minus-line.svg';
+import disabledMinus from '../../assets/icon-disabled-minus.svg';
 import plusIcon from '../../assets/icon-plus-line.svg';
 import disabledPlus from '../../assets/icon-disabled-plus.svg';
 
@@ -18,7 +19,10 @@ const AmountWrapper = styled.div`
 const Minus = styled.button`
     width: 100%;
     height: 100%;
-    background: url(${minusIcon}) no-repeat center;
+    background: url(${(props) => props.isOne ? disabledMinus : minusIcon}) no-repeat center;
+    background-color: var(${(props) => props.isOne ? '--drop-down': '--white'});
+    border-radius: 0px;
+
 `
 
 const AmountNum = styled.span`
@@ -41,9 +45,9 @@ const Plus = styled.button`
 const AmountBtn = ({num, isMax, handleMinus, handlePlus}) => {
     return (
         <AmountWrapper>
-            <Minus type='button' onClick={handleMinus} />
+            <Minus type='button' onClick={handleMinus} isOne={num === 1} disabled={num === 1}/>
             <AmountNum>{num}</AmountNum>
-            <Plus type='button' isMax={isMax} onClick={handlePlus}/>
+            <Plus type='button' isMax={isMax} onClick={handlePlus} disabled={isMax}/>
         </AmountWrapper>
     )
 };
