439710222f933eb28bf62aeeaacfd5e4c16f5ddd
“nimoseel” <fromsomini@gmail.com>
2023-12-06 22:37:28 +0900

    Refactor : useInput hook 사용, 훅 사용에 따른 코드 수정, post patch 함수 분리

--- src/pages/ProductUploadPage/ProductUpload.jsx
+++ src/pages/ProductUploadPage/ProductUpload.jsx
@@ -3,6 +3,7 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthContext';
 import { postProduct, editProduct } from '../../API/sellerApi';
 import Header from '../../components/Header/SellerCenterHeader';
+import useInput from '../../hooks/useInput';
 import * as S from '../ProductUploadPage/_style';
 
 const ProductUpload = () => {
@@ -13,73 +14,76 @@ const ProductUpload = () => {
     
     const [ methodChecked, setMethodChecked ] = useState(true);
     const [ thumbnail, setThumbnail ] = useState('');
-
-    const [ inputs, setInputs ] = useState({
-        product_name : '',
-        image : '',
-        price : '',
-        shipping_method : 'PARCEL',
-        shipping_fee : '',
-        stock : '',
-        product_info : '',
-    });
-
-    const { product_name, image, price, shipping_method, shipping_fee, stock, product_info } = inputs ;
+    
+    const [ image, setImage ] = useState('');
+    const [ shipping_method, setShipping_method ] = useState('PARCEL');
+    
+    const product_name = useInput('', null, 'product_name');
+    const price = useInput('', null, 'price');
+    const shipping_fee = useInput('', null, 'shipping_fee');
+    const stock = useInput('', null, 'stock');
+    const product_info = useInput('', null, 'product_info');
 
     useEffect(()=>{
         if(data){
-            setInputs({
-                product_name : data.product_name,
-                price : data.price,
-                shipping_method : data.shipping_method === 'DELIVERY' ? 'DELIVERY' : 'PARCEL',
-                shipping_fee : data.shipping_fee,
-                stock : data.stock,
-                product_info : data.product_info,
-            });
+            product_name.setValue(data.product_name);
+            price.setValue(data.price);
+            shipping_fee.setValue(data.shipping_fee);
+            stock.setValue(data.stock);
+            product_info.setValue(data.product_info);
+            setShipping_method(data.shipping_method === 'DELIVERY' ? 'DELIVERY' : 'PARCEL');
+            setImage(data.image);
             setThumbnail(data.image);
             setMethodChecked(data.shipping_method === 'DELIVERY' ? false : true);
+        }else{
+            return;
         }
-    },[data])
-
-    const onChange = (e) => {
-        setInputs({
-            ...inputs,
-            [e.target.name] : e.target.value,
-        });
-    }
+    },[])
 
     const handleShippingMethod = (e) => {
         setMethodChecked(!methodChecked);
-        onChange(e);
+        setShipping_method(e.target.value);
     }
 
     const onChangeFile = (e) => {
         if (e.target.files !== null) {
-                setInputs({
-                    ...inputs,
-                    image : e.target.files[0],
-                });
-            setThumbnail(URL.createObjectURL(e.target.files[0]));
-            URL.revokeObjectURL(thumbnail);
+            const file = e.target.files[0];
+            setImage(e.target.files[0]);
+            const thumbnailUrl = URL.createObjectURL(file);
+            setThumbnail(thumbnailUrl);
+
+            if (thumbnail) {
+                URL.revokeObjectURL(thumbnail);
+            }
         }
     }
     
     // post, patch
+    const reqData = {
+        'product_name' : product_name.value,
+        'price' : price.value,
+        'shipping_method' : shipping_method,
+        'shipping_fee' : shipping_fee.value || 0,
+        'stock' : stock.value,
+        'product_info' : product_info.value,
+    };
+
     const handlePostProduct = async() => {
-        const reqData = {
-            product_name,
-            image,
-            price,
-            shipping_method,
-            shipping_fee : shipping_fee || 0,
-            stock,
-            product_info,
-        };
+        Object.assign(reqData,{image});
 
         try{
-            const response = !data ? await postProduct(reqData, token) : await editProduct(reqData, data.product_id, token);
+            const response =  await postProduct(reqData, token);
             const product_id = response.data.product_id;
-            !data ? navigate(`/product/${product_id}`) : navigate('/sellercenter');
+            navigate(`/product/${product_id}`)
+        }catch(error){
+            console.error(error);
+        }
+    }
+
+    const handleEditProduct = async() => {
+        try{
+            await editProduct(reqData, data.product_id, token);
+            navigate('/sellercenter');
         }catch(error){
             console.error(error);
         }
@@ -87,11 +91,11 @@ const ProductUpload = () => {
 
     const isCommonValid = () => {
         return (
-            product_name &&
-            product_info &&
-            price > 0 &&
-            shipping_fee >= 0 &&
-            stock > 0
+            product_name.value &&
+            product_info.value &&
+            price.value > 0 &&
+            shipping_fee.value >= 0 &&
+            stock.value > 0
         );
     }
 
@@ -117,6 +121,7 @@ const ProductUpload = () => {
                     <S.CautionTitle>* 상품 등록 주의사항</S.CautionTitle>
                     <S.CautionContent>
                         - 상품명은 띄어쓰기 포함 최대 20자까지 설정 가능합니다.<br/><br/>
+                        - 상품 이미지는 수정할 수 없습니다.<br/><br/>
                         - 판매가는 0원으로 설정할 수 없습니다. 정확한 판매가를 기입해주시길 바랍니다.<br/><br/>
                         - 배송방법을 지정하지 않을 경우 택배, 소포, 등기로 등록됩니다.<br/><br/>
                         - 기본 배송비를 0원으로 설정할 경우 무료배송으로 등록됩니다.<br/><br/>
@@ -134,53 +139,52 @@ const ProductUpload = () => {
                                     <label htmlFor='file'>
                                         <S.ImgUploadBtn />
                                     </label>
-                                    <input type='file' id='file' name='image' accept='image/*' onChange={(e)=>{onChangeFile(e)}} style={{display:'none'}}/>
+                                    <input type='file' id='file' name='image' accept='image/*' onChange={onChangeFile} style={{display:'none'}}/>
                                 </S.ProductImgDiv>
                             </S.InputWrapper>
                         </div>
                         <div>
                             <S.InputWrapper>
                                 <S.InputTitle>상품명</S.InputTitle>
-                                <S.ProductNameInput name='product_name' value={product_name} onChange={onChange} />
+                                <S.ProductNameInput {...product_name} />
                             </S.InputWrapper>
                             <S.InputWrapper>
                                 <S.InputTitle>판매가</S.InputTitle>
-                                <S.StyledInput name='price' value={price} onChange={onChange} txt={'원'}/>
+                                <S.StyledInput {...price} txt={'원'}/>
                             </S.InputWrapper>
                             <S.InputWrapper>
                                 <S.InputTitle>배송방법</S.InputTitle>
                                 <S.BtnDiv>
-                                    <S.ShippingBtn name='shipping_method' value={'PARCEL'} isFocused={methodChecked} onClick={(e)=>handleShippingMethod(e)} >택배,소포,등기</S.ShippingBtn>
-                                    <S.ShippingBtn name='shipping_method' value={'DELIVERY'} isFocused={!methodChecked} onClick={(e)=>handleShippingMethod(e)}>직접배송(화물배달)</S.ShippingBtn>
+                                    <S.ShippingBtn name='shipping_method' value={'PARCEL'} isFocused={methodChecked} onClick={handleShippingMethod} >택배,소포,등기</S.ShippingBtn> 
+                                    <S.ShippingBtn name='shipping_method' value={'DELIVERY'} isFocused={!methodChecked} onClick={handleShippingMethod}>직접배송(화물배달)</S.ShippingBtn>
                                 </S.BtnDiv>
                             </S.InputWrapper>
                             <S.InputWrapper>
                                 <S.InputTitle>기본 배송비</S.InputTitle>
-                                <S.StyledInput name='shipping_fee' value={shipping_fee} onChange={onChange} txt={'원'}/>
+                                <S.StyledInput {...shipping_fee} txt={'원'}/>
+
                             </S.InputWrapper>
                             <S.InputWrapper>
                                 <S.InputTitle>재고</S.InputTitle>
-                                <S.StyledInput name='stock' value={stock} onChange={onChange} txt={'개'} />
+                                <S.StyledInput {...stock} txt={'개'} />
                             </S.InputWrapper>
                         </div>
                     </S.ProductMainInputDiv>
                     <div>
                         <S.InputWrapper>
                             <S.InputTitle>상품 상세 정보</S.InputTitle>
-                            <S.ProductInfoInput name='product_info' value={product_info} onChange={onChange} />
+                            <S.ProductInfoInput {...product_info} />
                         </S.InputWrapper>
                     </div>
                 </div>
             </S.MainDiv>
             <S.FinalBtnDiv>
                 <S.Btn type='white' onClick={()=>{navigate(-1)}}>취소</S.Btn>
-
-                { btnValid()  ?
-                    <S.Btn type='green' onClick={handlePostProduct}>저장하기</S.Btn>
+                { btnValid()?
+                    <S.Btn type='green' onClick={data ? handleEditProduct : handlePostProduct}>저장하기</S.Btn>
                     :
                     <S.Btn type='disabled' disabled>저장하기</S.Btn>
                 }
-
             </S.FinalBtnDiv>
         </>
     );
