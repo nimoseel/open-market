efa0d6e7b046a101e75cdee72f41517dc247c60c
“nimoseel” <fromsomini@gmail.com>
2024-01-08 22:13:02 +0900

    Style : 프리티어 적용

--- src/components/Paging/Paging.jsx
+++ src/components/Paging/Paging.jsx
@@ -4,14 +4,14 @@ import './_style.css';
 
 export default function Paging({ page, count, setPage }) {
     return (
-            <Pagination
-                activePage={page}
-                itemsCountPerPage={15}
-                totalItemsCount={count}
-                pageRangeDisplayed={5}
-                prevPageText={'<'}
-                nextPageText={'>'}
-                onChange={setPage}
-            />
+        <Pagination
+            activePage={page}
+            itemsCountPerPage={15}
+            totalItemsCount={count}
+            pageRangeDisplayed={5}
+            prevPageText={'<'}
+            nextPageText={'>'}
+            onChange={setPage}
+        />
     );
 }
