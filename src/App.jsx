9909769efb41eeb55461ee3cc4cb924319b7a4f5
“nimoseel” <fromsomini@gmail.com>
2024-01-08 22:08:13 +0900

    Style : 프리티어 적용

--- src/App.jsx
+++ src/App.jsx
@@ -13,26 +13,35 @@ import SellerCenter from './pages/seller/SellerCenterPage/SellerCenter';
 import ProductUpload from './pages/seller/ProductUploadPage/ProductUpload';
 
 function App() {
-  return (
-      <BrowserRouter>
-      <GlobalStyle/>
-        <div className='App'>
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/page/:page" element={<Home />}></Route>
-            <Route path='/product/:product_id' element={<Detail/>}></Route>
-            <Route path='/join' element={<Join/>}></Route>
-            <Route path='/login' element={<Login/>}></Route>
-            <Route path='/search' element={<SearchResult/>}></Route>
-            <Route path='/cart' element={<Cart/>}></Route>
-            <Route path='/payment' element={<Payment/>}></Route>
-            <Route path='/sellercenter' element={<SellerCenter/>}></Route>
-            <Route path='/productupload' element={<ProductUpload/>}></Route>
-            <Route path='/*' element={<NotFound/>}></Route>
-          </Routes>
-        </div>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <GlobalStyle />
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route path="/page/:page" element={<Home />}></Route>
+                    <Route
+                        path="/product/:product_id"
+                        element={<Detail />}
+                    ></Route>
+                    <Route path="/join" element={<Join />}></Route>
+                    <Route path="/login" element={<Login />}></Route>
+                    <Route path="/search" element={<SearchResult />}></Route>
+                    <Route path="/cart" element={<Cart />}></Route>
+                    <Route path="/payment" element={<Payment />}></Route>
+                    <Route
+                        path="/sellercenter"
+                        element={<SellerCenter />}
+                    ></Route>
+                    <Route
+                        path="/productupload"
+                        element={<ProductUpload />}
+                    ></Route>
+                    <Route path="/*" element={<NotFound />}></Route>
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
