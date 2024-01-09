d125124baa3ec130b42032eaadd1beec58426b03
“nimoseel” <fromsomini@gmail.com>
2024-01-09 23:35:43 +0900

    Style : 프리티어 적용

--- src/components/Etc/AmountBtn.jsx
+++ src/components/Etc/AmountBtn.jsx
@@ -14,15 +14,18 @@ const AmountWrapper = styled.div`
     border: 1px solid var(--disabled);
     border-radius: 5px;
     overflow: hidden;
-`
+`;
 
 const Minus = styled.button`
     width: 100%;
     height: 100%;
-    background: url(${(props) => props.isOne ? disabledMinus : minusIcon}) no-repeat center;
-    background-color: var(${(props) => props.isOne ? '--drop-down': '--white'});
+    background: url(${(props) => (props.isOne ? disabledMinus : minusIcon)})
+        no-repeat center;
+    background-color: var(
+        ${(props) => (props.isOne ? '--drop-down' : '--white')}
+    );
     border-radius: 0px;
-`
+`;
 
 const AmountNum = styled.span`
     display: inline-block;
@@ -32,23 +35,36 @@ const AmountNum = styled.span`
     font-size: 18px;
     line-height: 50px;
     text-align: center;
-`
+`;
 
 const Plus = styled.button`
     border-top-left-radius: 0;
     border-bottom-left-radius: 0;
-    background: url(${(props) => props.isMax ? disabledPlus : plusIcon}) no-repeat center;
-    background-color: var(${(props) => props.isMax ? '--drop-down': '--white'});
-`
+    background: url(${(props) => (props.isMax ? disabledPlus : plusIcon)})
+        no-repeat center;
+    background-color: var(
+        ${(props) => (props.isMax ? '--drop-down' : '--white')}
+    );
+`;
 
-const AmountBtn = ({num, isMax, handleMinus, handlePlus}) => {
+const AmountBtn = ({ num, isMax, handleMinus, handlePlus }) => {
     return (
         <AmountWrapper>
-            <Minus type='button' onClick={handleMinus} isOne={num === 1} disabled={num === 1}/>
+            <Minus
+                type="button"
+                onClick={handleMinus}
+                isOne={num === 1}
+                disabled={num === 1}
+            />
             <AmountNum>{num}</AmountNum>
-            <Plus type='button' isMax={isMax} onClick={handlePlus} disabled={isMax}/>
+            <Plus
+                type="button"
+                isMax={isMax}
+                onClick={handlePlus}
+                disabled={isMax}
+            />
         </AmountWrapper>
-    )
+    );
 };
 
 export default AmountBtn;
