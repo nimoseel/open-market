cfe0d96551d78374a76f312581bf42b831ea34c1
“nimoseel” <fromsomini@gmail.com>
2024-01-09 23:35:48 +0900

    Style : 프리티어 적용

--- src/components/Etc/Button.jsx
+++ src/components/Etc/Button.jsx
@@ -3,24 +3,28 @@ import styled from 'styled-components';
 const Btn_common = styled.button`
     display: inline-block;
     border: ${(props) => props.type === 'white' && '1px solid var(--disabled)'};
-    color : var(${(props) => props.type === 'white' ? '--dark-gray': '--white'});
-    background-color: 
-        var(${(props) => 
+    color: var(
+        ${(props) => (props.type === 'white' ? '--dark-gray' : '--white')}
+    );
+    background-color: var(
+        ${(props) =>
             (props.type === 'disabled' && '--disabled') ||
             (props.type === 'dark' && '--dark-gray') ||
             (props.type === 'white' && '--white') ||
-            ('--main')});
+            '--main'}
+    );
     &:hover {
-        border: ${(props) => props.type === 'white' && '1px solid var(--dark-gray)'};
+        border: ${(props) =>
+            props.type === 'white' && '1px solid var(--dark-gray)'};
         color: ${(props) => props.type === 'white' && 'var(--black)'};
     }
-`
+`;
 
 export const BtnL = styled(Btn_common)`
     height: 68px;
     font-size: 24px;
     font-weight: var(--bold);
-`
+`;
 
 export const BtnM = styled(Btn_common)`
     width: 100%;
@@ -29,32 +33,38 @@ export const BtnM = styled(Btn_common)`
     font-size: 18px;
     font-weight: var(--bold);
     &:hover {
-        border: ${(props) => props.type === 'white' && '1px solid var(--disabled)'};
-        color: var(${(props) => props.type === 'white' ? '--dark-gray': '--white'});
+        border: ${(props) =>
+            props.type === 'white' && '1px solid var(--disabled)'};
+        color: var(
+            ${(props) => (props.type === 'white' ? '--dark-gray' : '--white')}
+        );
     }
-`
+`;
 
 export const BtnMS = styled(Btn_common)`
     height: 54px;
     font-weight: var(--semi-bold);
     font-size: 16px;
-`
+`;
 
 export const BtnS = styled(Btn_common)`
     height: 40px;
     font-weight: var(--semi-bold);
     font-size: 16px;
-`
+`;
 
 export const BtnTab = styled.button`
     width: 320px;
     height: 54px;
     border-radius: 0;
-    border-bottom: 6px solid var(${(props) => props.type === 'disabled' ? '--disabled': '--main'});
-    color: var(${(props) => props.type === 'disabled' ? '--dark-gray': '--main'});
+    border-bottom: 6px solid
+        var(${(props) => (props.type === 'disabled' ? '--disabled' : '--main')});
+    color: var(
+        ${(props) => (props.type === 'disabled' ? '--dark-gray' : '--main')}
+    );
     background-color: var(--white);
     font-size: 18px;
-    &:focus{
+    &:focus {
         color: var(--main);
     }
-`
+`;
