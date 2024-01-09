8939ba867cfc8ce8f2050357576c35cd046df319
“nimoseel” <fromsomini@gmail.com>
2024-01-09 23:35:36 +0900

    Style : 프리티어 적용

--- src/components/Etc/Price.jsx
+++ src/components/Etc/Price.jsx
@@ -3,34 +3,38 @@ import styled from 'styled-components';
 
 const StyledPrice = styled.span`
     display: inline-block;
-    margin-right: ${props => `${props.margin}px`};
+    margin-right: ${(props) => `${props.margin}px`};
     font-weight: var(--bold);
-    font-size: ${props => `${props.numsize}px`};
-    color: var(${props => `${props.color}`});
+    font-size: ${(props) => `${props.numsize}px`};
+    color: var(${(props) => `${props.color}`});
 
     @media screen and (max-width: 768px) {
-        font-size: ${props => `${props.numsize * 0.8}px`}
+        font-size: ${(props) => `${props.numsize * 0.8}px`};
     }
-`
+`;
 
 const StyledTxt = styled.span`
-    font-size: ${props => `${props.txtsize}px`};
+    font-size: ${(props) => `${props.txtsize}px`};
     font-weight: var(--regular);
-    color: var(${props => `${props.color}`});
+    color: var(${(props) => `${props.color}`});
     color: var(--black);
 
     @media screen and (max-width: 768px) {
         font-size: 16px;
     }
-`
+`;
 
-export default function Price ({numsize, margin, color, price, txtsize}){
+export default function Price({ numsize, margin, color, price, txtsize }) {
     return (
         <>
             <StyledPrice numsize={numsize} margin={margin} color={color}>
-                {price.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')}
+                {price
+                    .toString()
+                    .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')}
             </StyledPrice>
-            <StyledTxt txtsize={txtsize} color={color}>원</StyledTxt>
+            <StyledTxt txtsize={txtsize} color={color}>
+                원
+            </StyledTxt>
         </>
-    )
-};
+    );
+}
