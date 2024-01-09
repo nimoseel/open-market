6337472c3539f39aacfd217b6b83970e5d0d6e16
“nimoseel” <fromsomini@gmail.com>
2024-01-09 23:26:06 +0900

    Style : 프리티어 적용

--- src/pages/common/404Page/NotFound.jsx
+++ src/pages/common/404Page/NotFound.jsx
@@ -8,14 +8,20 @@ const NotFound = () => {
 
     return (
         <S.Section>
-            <img src={Icon404} alt='404이미지' />
+            <img src={Icon404} alt="404이미지" />
             <div>
                 <S.H2>페이지를 찾을 수 없습니다.</S.H2>
-                <S.Txt>페이지가 존재하지 않거나 사용할 수 없는 페이지입니다.<br/>
-                웹 주소가 올바른지 확인해 주세요.</S.Txt>
+                <S.Txt>
+                    페이지가 존재하지 않거나 사용할 수 없는 페이지입니다.
+                    <br />웹 주소가 올바른지 확인해 주세요.
+                </S.Txt>
                 <S.BtnDiv>
-                    <S.Btn type={'green'} onClick={()=>navigate('/')}>메인으로</S.Btn>
-                    <S.Btn type={'white'} onClick={()=>navigate(-1)}>이전 페이지</S.Btn>
+                    <S.Btn type={'green'} onClick={() => navigate('/')}>
+                        메인으로
+                    </S.Btn>
+                    <S.Btn type={'white'} onClick={() => navigate(-1)}>
+                        이전 페이지
+                    </S.Btn>
                 </S.BtnDiv>
             </div>
         </S.Section>
--- src/pages/common/404Page/_style.jsx
+++ src/pages/common/404Page/_style.jsx
@@ -7,25 +7,25 @@ export const Section = styled.section`
     margin: 300px auto 0;
     align-items: center;
     gap: 50px;
-`
+`;
 
 export const H2 = styled.h2`
     font-weight: var(--bold);
     font-size: 36px;
-`
+`;
 
 export const Txt = styled.p`
     margin: 20px 0 40px;
     font-weight: var(--regular);
     color: var(--dark-gray);
     line-height: 20.03px;
-`
+`;
 
 export const BtnDiv = styled.div`
     display: flex;
     gap: 14px;
-`
+`;
 
 export const Btn = styled(BtnM)`
     width: 200px;
-`
+`;
