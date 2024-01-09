2756105b2c9c1bcbad92fe69cfc2f9a113fcb7b4
“nimoseel” <fromsomini@gmail.com>
2024-01-09 23:35:53 +0900

    Style : 프리티어 적용

--- src/components/Etc/CartCheckBox.jsx
+++ src/components/Etc/CartCheckBox.jsx
@@ -6,15 +6,15 @@ const CartCheckLabel = styled.label`
     display: inline-block;
     min-width: 22px;
     min-height: 22px;
-    background-image: url(${(props) => props.isCheck ? Checked : UnCheck});
+    background-image: url(${(props) => (props.isCheck ? Checked : UnCheck)});
     background-repeat: no-repeat;
     background-size: contain;
     cursor: pointer;
-`
+`;
 
 const CartCheckInput = styled.input`
     display: none;
-`
+`;
 
 const CartCheckBox = ({ isCheck, setIsCheck, handleClick }) => {
     const handleCheckBoxClick = () => {
@@ -26,10 +26,14 @@ const CartCheckBox = ({ isCheck, setIsCheck, handleClick }) => {
 
     return (
         <>
-            <CartCheckLabel htmlFor='chk_box' isCheck={isCheck} onClick={handleCheckBoxClick} />
-            <CartCheckInput type='checkbox' id='chk_box'/>   
+            <CartCheckLabel
+                htmlFor="chk_box"
+                isCheck={isCheck}
+                onClick={handleCheckBoxClick}
+            />
+            <CartCheckInput type="checkbox" id="chk_box" />
         </>
-    )
+    );
 };
 
 export default CartCheckBox;
