d013323e249d8211771739893424cbbfe64decea
“nimoseel” <fromsomini@gmail.com>
2024-01-09 23:25:14 +0900

    Style : 프리티어 적용

--- src/pages/common/SearchResultPage/SearchResult.jsx
+++ src/pages/common/SearchResultPage/SearchResult.jsx
@@ -1,31 +1,31 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
-import { getSearchData } from "../../../API/searchApi";
-import Header from "../../../components/Header/Header";
-import Footer from "../../../components/Footer/Footer";
+import { getSearchData } from '../../../API/searchApi';
+import Header from '../../../components/Header/Header';
+import Footer from '../../../components/Footer/Footer';
 import Loading from '../../../components/Loading/Loading';
 import Paging from '../../../components/Paging/Paging';
 import ProductCard from '../../../components/ProductCard/ProductCard';
-import * as S from '../SearchResultPage/_style'
+import * as S from '../SearchResultPage/_style';
 
 const SearchResult = () => {
     const navigate = useNavigate();
-    const location = useLocation(); 
+    const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
     const searchValue = queryParams.get('search');
     const page = queryParams.get('page');
 
-    const [ searchData, setSearchData ] = useState({});
-    const [ searchResults, setSearchResults ] = useState([]);
-    const [ activePage, setActivePage ] = useState(parseInt(page) || 1);
-    const [ loading, setLoading ] = useState(false);
+    const [searchData, setSearchData] = useState({});
+    const [searchResults, setSearchResults] = useState([]);
+    const [activePage, setActivePage] = useState(parseInt(page) || 1);
+    const [loading, setLoading] = useState(false);
 
-    useEffect(()=>{
+    useEffect(() => {
         setLoading(true);
-        if (searchValue){
+        if (searchValue) {
             try {
                 const encodedSearchValue = encodeURIComponent(searchValue);
-                getSearchData(activePage, encodedSearchValue).then(res => {
+                getSearchData(activePage, encodedSearchValue).then((res) => {
                     setSearchData(res.data);
                     setSearchResults(res.data.results);
                     setLoading(false);
@@ -33,12 +33,12 @@ const SearchResult = () => {
             } catch (error) {
                 console.error(error);
             }
-        }else{
+        } else {
             setSearchData({});
             setSearchResults([]);
             setLoading(false);
         }
-    },[searchValue, activePage]);
+    }, [searchValue, activePage]);
 
     const handlePageChange = (e) => {
         setActivePage(e);
@@ -50,49 +50,44 @@ const SearchResult = () => {
 
     return (
         <>
-            <Header/>
-            {loading && <Loading/>}
+            <Header />
+            {loading && <Loading />}
             <S.Main>
                 <S.SearchCount>
-                    <S.SearchTxt>
-                        {searchValue}
-                    </S.SearchTxt>
+                    <S.SearchTxt>{searchValue}</S.SearchTxt>
                     <S.SearchResultTxt>
-                        {searchCount ? 
-                            `총 ${searchCount}개의 상품이 검색되었습니다.` 
-                            : 
-                            '앗! 찾으시는 상품이 없네요.'
-                        }
+                        {searchCount
+                            ? `총 ${searchCount}개의 상품이 검색되었습니다.`
+                            : '앗! 찾으시는 상품이 없네요.'}
                     </S.SearchResultTxt>
                 </S.SearchCount>
-                {!loading && (!searchCount || !searchValue)? 
-                    <S.NoResult/>
-                    :
+                {!loading && (!searchCount || !searchValue) ? (
+                    <S.NoResult />
+                ) : (
                     <S.ProductUl className={loading ? '' : 'show'}>
-                        {searchResults.map((item) => 
-                            <Link 
-                                key={item.product_id} 
+                        {searchResults.map((item) => (
+                            <Link
+                                key={item.product_id}
                                 to={`/product/${item.product_id}`}
                             >
-                                <ProductCard
-                                    {...item}
-                                    key={item.product_id}
-                                    />
+                                <ProductCard {...item} key={item.product_id} />
                             </Link>
-                        )}
+                        ))}
                     </S.ProductUl>
-                }
+                )}
             </S.Main>
-            {searchCount > 15 &&
+            {searchCount > 15 && (
                 <Paging
                     page={activePage}
                     count={searchCount}
                     setPage={handlePageChange}
                 />
-            }
-            <Footer/>
+            )}
+
+            {!loading && <Footer />}
+            {/* <Footer /> */}
         </>
-    )
+    );
 };
 
 export default SearchResult;
