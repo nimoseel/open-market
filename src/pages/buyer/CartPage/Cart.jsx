ba8665e632dde62f4495f7c3d33decaa4c1208de
“nimoseel” <fromsomini@gmail.com>
2024-01-09 23:27:21 +0900

    Style : 프리티어 적용

--- src/pages/buyer/CartPage/Cart.jsx
+++ src/pages/buyer/CartPage/Cart.jsx
@@ -13,26 +13,26 @@ import * as S from '../CartPage/_style';
 const Cart = () => {
     const { token } = useContext(AuthContext);
     const navigate = useNavigate();
-    const [ cartLists, setCartLists] = useState([]);
-    const [ loading, setLoading ] = useState(null);
-    const [ isAllChecked, setIsAllChecked ] = useState(true);
-    
-    useEffect(()=>{
+    const [cartLists, setCartLists] = useState([]);
+    const [loading, setLoading] = useState(null);
+    const [isAllChecked, setIsAllChecked] = useState(true);
+
+    useEffect(() => {
         setLoading(true);
-        
-        const fetchCartData = async() => {
+
+        const fetchCartData = async () => {
             try {
                 // 1. 장바구니 정보 가져오기
                 const cartData = await getCart(token);
                 const cartResults = cartData.results;
 
-                // 2. 장바구니 상품 상세 정보 가져오기, 비동기 작업 병렬 처리 
+                // 2. 장바구니 상품 상세 정보 가져오기, 비동기 작업 병렬 처리
                 const getCartDetail = cartResults.map(async (item) => {
                     return await getDetail(item.product_id);
                 });
                 const resolvedCartDetail = await Promise.all(getCartDetail);
 
-                // 3. 장바구니 상품 정보 업데이트 
+                // 3. 장바구니 상품 정보 업데이트
                 const updatedCartList = cartResults.map((item, index) => ({
                     ...item,
                     ...resolvedCartDetail[index],
@@ -40,47 +40,57 @@ const Cart = () => {
                 setCartLists(updatedCartList);
 
                 // 4. 체크 박스 값 확인
-                const isAllItemsChecked = updatedCartList.every(item => item.is_active);
+                const isAllItemsChecked = updatedCartList.every(
+                    (item) => item.is_active,
+                );
                 setIsAllChecked(isAllItemsChecked);
-                
+
                 setLoading(false);
             } catch (error) {
                 console.error(error);
             }
         };
         fetchCartData();
-    },[token]);
-    
-    const calculateTotal = (array) => array.reduce((acc, cur) => acc+cur, 0);
-    const totalPrice = calculateTotal(cartLists.map(item => item.is_active && item.price * item.quantity));
-    const totalFee = calculateTotal(cartLists.map(item => item.is_active && item.shipping_fee));
+    }, [token]);
+
+    const calculateTotal = (array) => array.reduce((acc, cur) => acc + cur, 0);
+    const totalPrice = calculateTotal(
+        cartLists.map((item) => item.is_active && item.price * item.quantity),
+    );
+    const totalFee = calculateTotal(
+        cartLists.map((item) => item.is_active && item.shipping_fee),
+    );
 
     const navigateToPayment = () => {
         const selected = cartLists.filter(
-            item => (item.is_active === true) && (item.order_kind = 'cart_order'));
+            (item) =>
+                item.is_active === true && (item.order_kind = 'cart_order'),
+        );
         navigate('/payment', {
-            state : { 
-                order_data : selected
+            state: {
+                order_data: selected,
             },
         });
-    }
+    };
 
     const updateAllCheckStatus = (updatedCartLists) => {
-        const areAllItemsChecked = updatedCartLists.every(item => item.is_active);
+        const areAllItemsChecked = updatedCartLists.every(
+            (item) => item.is_active,
+        );
         setIsAllChecked(areAllItemsChecked);
-    }
+    };
 
     const handleAllItemsCheck = () => {
-        const updatedCartLists = cartLists.map(item => ({
+        const updatedCartLists = cartLists.map((item) => ({
             ...item,
-            is_active: !isAllChecked, 
+            is_active: !isAllChecked,
         }));
-        setCartLists(updatedCartLists); 
+        setCartLists(updatedCartLists);
         setIsAllChecked(!isAllChecked);
-    }
+    };
 
     const handleItemCheck = (productId) => {
-        const updatedCartLists = cartLists.map(item => {
+        const updatedCartLists = cartLists.map((item) => {
             if (item.product_id === productId) {
                 return {
                     ...item,
@@ -89,54 +99,58 @@ const Cart = () => {
             }
             return item;
         });
-        setCartLists(updatedCartLists); 
+        setCartLists(updatedCartLists);
         updateAllCheckStatus(updatedCartLists);
-    }
+    };
 
-    const isAnyItemSelected = () => cartLists.some(x => x.is_active);
+    const isAnyItemSelected = () => cartLists.some((x) => x.is_active);
 
     return (
         <>
-            {loading && <Loading/>}
-            <Header/>
+            {loading && <Loading />}
+            <Header />
             <S.CartTitleDiv>
                 <S.CartHeader>장바구니</S.CartHeader>
                 <S.MenuUl>
-                        <CartCheckBox
-                            isCheck={isAllChecked}
-                            setIsCheck={setIsAllChecked}
-                            handleClick={handleAllItemsCheck}
-                        />
+                    <CartCheckBox
+                        isCheck={isAllChecked}
+                        setIsCheck={setIsAllChecked}
+                        handleClick={handleAllItemsCheck}
+                    />
                     <S.MenuLi>상품정보</S.MenuLi>
                     <S.MenuLi>수량</S.MenuLi>
                     <S.MenuLi>상품금액</S.MenuLi>
                 </S.MenuUl>
             </S.CartTitleDiv>
-            {!loading && cartLists && cartLists.length === 0 ? 
+            {!loading && cartLists && cartLists.length === 0 ? (
                 <S.EmptyDiv className={loading ? '' : 'show'}>
-                    <S.EmptyBoldTxt>장바구니에 담긴 상품이 없습니다.</S.EmptyBoldTxt>
-                    <S.EmptyTxt>원하는 상품을 장바구니에 담아보세요!</S.EmptyTxt>
+                    <S.EmptyBoldTxt>
+                        장바구니에 담긴 상품이 없습니다.
+                    </S.EmptyBoldTxt>
+                    <S.EmptyTxt>
+                        원하는 상품을 장바구니에 담아보세요!
+                    </S.EmptyTxt>
                 </S.EmptyDiv>
-                :
+            ) : (
                 <S.CartContentDiv className={loading ? '' : 'show'}>
                     {cartLists.map((item) => (
-                        <CartItem 
-                            {...item} 
-                            key={item.product_id} 
+                        <CartItem
+                            {...item}
+                            key={item.product_id}
                             isChecked={item.is_active}
                             handleItemCheck={handleItemCheck}
                         />
                     ))}
-                    <CartTotal totalPrice={totalPrice} totalFee={totalFee}/>
-                    <S.OrderBtn 
-                        type={isAnyItemSelected() ? 'green' : 'disabled'} 
-                        disabled={!isAnyItemSelected()} 
+                    <CartTotal totalPrice={totalPrice} totalFee={totalFee} />
+                    <S.OrderBtn
+                        type={isAnyItemSelected() ? 'green' : 'disabled'}
+                        disabled={!isAnyItemSelected()}
                         onClick={isAnyItemSelected() ? navigateToPayment : null}
                     >
-                    주문하기
+                        주문하기
                     </S.OrderBtn>
                 </S.CartContentDiv>
-            }
+            )}
         </>
     );
 };
--- src/pages/buyer/CartPage/_style.jsx
+++ src/pages/buyer/CartPage/_style.jsx
@@ -6,7 +6,7 @@ export const CartTitleDiv = styled.div`
     max-width: 1280px;
     margin: 0 auto 35px;
     text-align: center;
-`
+`;
 
 export const CartHeader = styled.h1`
     display: inline-block;
@@ -18,7 +18,7 @@ export const CartHeader = styled.h1`
     @media screen and (max-width: 768px) {
         font-size: 22px;
     }
-`
+`;
 
 export const MenuUl = styled.ul`
     display: flex;
@@ -34,7 +34,7 @@ export const MenuUl = styled.ul`
     font-size: 18px;
     line-height: 22.54px;
     background-color: var(--menu);
-    
+
     @media screen and (max-width: 1280px) {
         width: 900px;
     }
@@ -42,60 +42,61 @@ export const MenuUl = styled.ul`
     @media screen and (max-width: 768px) {
         width: 450px;
     }
-`
+`;
 
 export const MenuLi = styled.li`
     font-size: 18px;
     font-weight: var(--regular);
     line-height: 22.54px;
 
-    &:nth-child(3){
+    &:nth-child(3) {
         width: 280px;
-        
+
         @media screen and (max-width: 1280px) {
             width: 190px;
         }
-    };
-
+    }
 
     @media screen and (max-width: 768px) {
         display: none;
     }
-`
+`;
 
 export const CartContentDiv = styled.div`
-    transition: opacity 1s, transform 1s;
+    transition:
+        opacity 1s,
+        transform 1s;
     transform: translateY(10px);
     opacity: 0;
-    
+
     &.show {
         transform: translateY(0);
         opacity: 1;
     }
-`
+`;
 
 export const EmptyDiv = styled(CartContentDiv)`
     width: 250px;
     margin: 200px auto 0;
     text-align: center;
-`
+`;
 
 export const EmptyBoldTxt = styled.p`
     margin-bottom: 17px;
     font-size: 18px;
     font-weight: var(--bold);
     line-height: 22.54px;
-`
+`;
 
 export const EmptyTxt = styled.p`
     font-size: 14px;
     font-weight: var(--regular);
     line-height: 17.53px;
     color: var(--dark-gray);
-`
+`;
 
 export const OrderBtn = styled(BtnL)`
     display: block;
     width: 220px;
     margin: 0 auto 60px;
-`
+`;
