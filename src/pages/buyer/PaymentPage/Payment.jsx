ebdc38dc288c84548ca0590476c7abe0865b8d84
“nimoseel” <fromsomini@gmail.com>
2024-01-09 23:26:15 +0900

    Style : 프리티어 적용

--- src/pages/buyer/PaymentPage/Payment.jsx
+++ src/pages/buyer/PaymentPage/Payment.jsx
@@ -32,39 +32,51 @@ const Payment = () => {
     const receiver_mobile_3 = useInput('', null, 'receiver_mobile_3');
     const post_code = useInput('', null, 'post_code');
     const address_1 = useInput('', null, 'address_1');
-    const address_2 = useInput('', null, 'address_2'); 
+    const address_2 = useInput('', null, 'address_2');
     const delivery_message = useInput('', null, 'delivery_message');
 
-    const orderer_mobile = orderer_mobile_1.value + orderer_mobile_2.value + orderer_mobile_3.value;
-    const receiver_mobile = receiver_mobile_1.value + receiver_mobile_2.value +receiver_mobile_3.value;
+    const orderer_mobile =
+        orderer_mobile_1.value +
+        orderer_mobile_2.value +
+        orderer_mobile_3.value;
+    const receiver_mobile =
+        receiver_mobile_1.value +
+        receiver_mobile_2.value +
+        receiver_mobile_3.value;
 
     // 버튼 관련 상태
-    const [ payment, setPayment ] = useState('');
-    const [ isOpenPostCode, setIsOpenPostCode ] = useState(false);
-    const [ isCheckAgree, setIsCheckAgree ] = useState(false);
+    const [payment, setPayment] = useState('');
+    const [isOpenPostCode, setIsOpenPostCode] = useState(false);
+    const [isCheckAgree, setIsCheckAgree] = useState(false);
 
     // 금액 계산
-    const calculateTotal = (array) => array.reduce((acc, cur) => acc+cur, 0);
-    const total_product_price = calculateTotal(data.map(i => i.price * i.quantity));
-    const total_shipping_fee = calculateTotal(data.map(i => i.shipping_fee));
-    const total_price = total_product_price + total_shipping_fee ;
-    
+    const calculateTotal = (array) => array.reduce((acc, cur) => acc + cur, 0);
+    const total_product_price = calculateTotal(
+        data.map((i) => i.price * i.quantity),
+    );
+    const total_shipping_fee = calculateTotal(data.map((i) => i.shipping_fee));
+    const total_price = total_product_price + total_shipping_fee;
+
     // 결제 수단
     const handlePayment = (e) => {
         setPayment(e.target.value);
-    }
+    };
 
     // 우편번호 검색
     const onCompletePost = (post) => {
         post_code.setValue(post.zonecode);
-        address_1.setValue(post.buildingName ? post.address + ' ('+ post.buildingName +')' : post.address);
+        address_1.setValue(
+            post.buildingName
+                ? post.address + ' (' + post.buildingName + ')'
+                : post.address,
+        );
         setIsOpenPostCode(false);
-    }
+    };
 
     const postCodeStyle = {
         width: '450px',
         height: '450px',
-    }
+    };
 
     // 모든 input 확인 후 버튼 활성화
     const isAllInputsValid = () => {
@@ -74,21 +86,24 @@ const Payment = () => {
             orderer_email.value.match(regex.email),
             receiver_name.value,
             receiver_mobile.match(regex.mobile),
-            post_code && address_1.value && address_2.value && delivery_message.value,
+            post_code &&
+                address_1.value &&
+                address_2.value &&
+                delivery_message.value,
             payment,
-            isCheckAgree
+            isCheckAgree,
         ];
 
-        return inputs.every(input => !!input);
-    }
+        return inputs.every((input) => !!input);
+    };
 
     // 결제하기
-    const postOrderData = async() => {
+    const postOrderData = async () => {
         const product_id = data[0].product_id;
         const quantity = data[0].quantity;
         const order_kind = data[0].order_kind;
         const receiver_phone_number = receiver_mobile;
-        const address = address_1.value+ ' '+ address_2.value;
+        const address = address_1.value + ' ' + address_2.value;
         const address_message = delivery_message.value;
         const receiver = receiver_name.value;
         const payment_method = payment;
@@ -112,14 +127,14 @@ const Payment = () => {
             receiver_phone_number,
             address,
             address_message,
-            payment_method, 
+            payment_method,
         };
 
         const cart_one_order_data = {
             product_id,
-            quantity, 		
+            quantity,
             order_kind,
-            total_price,   
+            total_price,
             receiver,
             receiver_phone_number,
             address,
@@ -127,139 +142,237 @@ const Payment = () => {
             payment_method,
         };
 
-        try{
-            if(order_kind === 'direct_order'){
+        try {
+            if (order_kind === 'direct_order') {
                 await postOrder(direct_order_data, token);
             }
-            if(order_kind === 'cart_order'){
+            if (order_kind === 'cart_order') {
                 await postOrder(cart_order_data, token);
             }
-            if(order_kind === 'cart_one_order'){
+            if (order_kind === 'cart_one_order') {
                 postOrder(cart_one_order_data, token);
             }
             alert('주문이 성공적으로 완료되었습니다 !');
             navigate('/');
-        }catch(error){
+        } catch (error) {
             console.error(error);
         }
-    }
+    };
 
     const handlePostCodeModal = () => {
         setIsOpenPostCode(!isOpenPostCode);
-    }
+    };
 
     return (
         <>
-            <Header/>
+            <Header />
             <S.PaymentTitleDiv>
                 <S.PaymentHeader>주문/결제하기</S.PaymentHeader>
             </S.PaymentTitleDiv>
             <S.ItemUl>
-                {data?.map((item) => 
-                    <PaymentItem
-                        {...item}
-                        key={item.product_id}
-                    />
-                )}
+                {data?.map((item) => (
+                    <PaymentItem {...item} key={item.product_id} />
+                ))}
             </S.ItemUl>
             <S.TotalPriceDiv>
                 <S.TotalSpan>총 주문금액</S.TotalSpan>
-                <Price price={total_price} numsize={24} margin={0} txtsize={24} color={'--main'}/>
+                <Price
+                    price={total_price}
+                    numsize={24}
+                    margin={0}
+                    txtsize={24}
+                    color={'--main'}
+                />
             </S.TotalPriceDiv>
             <S.InfoInputDiv>
                 <S.InfoTitle>배송정보</S.InfoTitle>
-                    <S.BoldHr/>
+                <S.BoldHr />
 
                 <S.InfoSubTitle>주문자 정보</S.InfoSubTitle>
-                    <S.BoldHr/>
+                <S.BoldHr />
 
                 <S.InputTitle>이름</S.InputTitle>
-                    <S.PaymentInput type='text' maxLength='5' {...orderer}/>
-                    <S.Hr/>
+                <S.PaymentInput type="text" maxLength="5" {...orderer} />
+                <S.Hr />
 
                 <S.InputTitle>휴대폰</S.InputTitle>
-                    <S.MobileInput1 type='tel' pattern='01(0|1|6|7|8|9)' maxLength='3' {...orderer_mobile_1}/><S.Hyphen/>
-                    <S.MobileInput type='tel' pattern='[0-9]{3,4}' maxLength='4' {...orderer_mobile_2}/><S.Hyphen/>
-                    <S.MobileInput type='tel' pattern='[0-9]{4}' maxLength='4' {...orderer_mobile_3}/>
-                    <S.Hr/>
+                <S.MobileInput1
+                    type="tel"
+                    pattern="01(0|1|6|7|8|9)"
+                    maxLength="3"
+                    {...orderer_mobile_1}
+                />
+                <S.Hyphen />
+                <S.MobileInput
+                    type="tel"
+                    pattern="[0-9]{3,4}"
+                    maxLength="4"
+                    {...orderer_mobile_2}
+                />
+                <S.Hyphen />
+                <S.MobileInput
+                    type="tel"
+                    pattern="[0-9]{4}"
+                    maxLength="4"
+                    {...orderer_mobile_3}
+                />
+                <S.Hr />
 
                 <S.InputTitle>이메일</S.InputTitle>
-                    <S.EmailInput type='email' pattern={regex.email} {...orderer_email}/>
-                    <S.Hr/>
+                <S.EmailInput
+                    type="email"
+                    pattern={regex.email}
+                    {...orderer_email}
+                />
+                <S.Hr />
 
                 <S.InfoSubTitle>배송지 정보</S.InfoSubTitle>
-                    <S.BoldHr/>
+                <S.BoldHr />
 
                 <S.InputTitle>수령인</S.InputTitle>
-                    <S.PaymentInput type='text' maxLength='5' {...receiver_name}/>
-                    <S.Hr/>
+                <S.PaymentInput type="text" maxLength="5" {...receiver_name} />
+                <S.Hr />
 
                 <S.InputTitle>휴대폰</S.InputTitle>
-                    <S.MobileInput1 type='tel' pattern='01(0|1|6|7|8|9)' maxLength='3' {...receiver_mobile_1}/><S.Hyphen/>
-                    <S.MobileInput type='tel' pattern='[0-9]{3,4}' maxLength='4' {...receiver_mobile_2}/><S.Hyphen/>
-                    <S.MobileInput type='tel' pattern='[0-9]{4}' maxLength='4' {...receiver_mobile_3}/>
-                    <S.Hr/>
+                <S.MobileInput1
+                    type="tel"
+                    pattern="01(0|1|6|7|8|9)"
+                    maxLength="3"
+                    {...receiver_mobile_1}
+                />
+                <S.Hyphen />
+                <S.MobileInput
+                    type="tel"
+                    pattern="[0-9]{3,4}"
+                    maxLength="4"
+                    {...receiver_mobile_2}
+                />
+                <S.Hyphen />
+                <S.MobileInput
+                    type="tel"
+                    pattern="[0-9]{4}"
+                    maxLength="4"
+                    {...receiver_mobile_3}
+                />
+                <S.Hr />
 
                 <S.AddressDiv>
                     <S.AddressTitle>배송주소</S.AddressTitle>
                     <div>
-                        <S.PostNumInput placeholder='우편번호' {...post_code} disabled/>
-                        <S.PostNumSearchBtn onClick={handlePostCodeModal}>우편번호 조회</S.PostNumSearchBtn>
-                        <S.AddressInput placeholder='주소' {...address_1} disabled/>
-                        <S.AddressInput placeholder='상세주소' {...address_2}/>
+                        <S.PostNumInput
+                            placeholder="우편번호"
+                            {...post_code}
+                            disabled
+                        />
+                        <S.PostNumSearchBtn onClick={handlePostCodeModal}>
+                            우편번호 조회
+                        </S.PostNumSearchBtn>
+                        <S.AddressInput
+                            placeholder="주소"
+                            {...address_1}
+                            disabled
+                        />
+                        <S.AddressInput placeholder="상세주소" {...address_2} />
                     </div>
 
-                    {isOpenPostCode && 
+                    {isOpenPostCode && (
                         <ModalPortal>
                             <S.ModalBg isOpenPostCode={isOpenPostCode}>
                                 <S.PostCodeContent>
                                     <S.PostCodeTitle>주소찾기</S.PostCodeTitle>
-                                    <S.DeleteBtn onClick={handlePostCodeModal}/>
-                                    <DaumPostCode style={postCodeStyle} autoClose onComplete={onCompletePost}/>
+                                    <S.DeleteBtn
+                                        onClick={handlePostCodeModal}
+                                    />
+                                    <DaumPostCode
+                                        style={postCodeStyle}
+                                        autoClose
+                                        onComplete={onCompletePost}
+                                    />
                                 </S.PostCodeContent>
                             </S.ModalBg>
                         </ModalPortal>
-                    }
-
+                    )}
                 </S.AddressDiv>
-                    <S.Hr/>
-                    <S.InputTitle>배송 메시지</S.InputTitle>
-                    <S.ShippingMsgInput {...delivery_message}/>
-                    <S.Hr/>
+                <S.Hr />
+                <S.InputTitle>배송 메시지</S.InputTitle>
+                <S.ShippingMsgInput {...delivery_message} />
+                <S.Hr />
             </S.InfoInputDiv>
 
             <S.PaymentDiv>
                 <S.MethodDiv>
                     <S.InfoTitle>결제수단</S.InfoTitle>
-                    <S.BoldHr/>
+                    <S.BoldHr />
                     <S.MethodUl>
-                        <S.MethodItem itemName={'CARD'} content={'신용/체크카드'} onClick={handlePayment}/>
-                        <S.MethodItem itemName={'DEPOSIT'} content={'무통장 입금'} onClick={handlePayment}/>
-                        <S.MethodItem itemName={'PHONE_PAYMENT'} content={'휴대폰 결제'} onClick={handlePayment}/>
-                        <S.MethodItem itemName={'NAVERPAY'} content={'네이버페이'} onClick={handlePayment}/>
-                        <S.MethodItem itemName={'KAKAOPAY'} content={'카카오페이'} onClick={handlePayment}/>
+                        <S.MethodItem
+                            itemName={'CARD'}
+                            content={'신용/체크카드'}
+                            onClick={handlePayment}
+                        />
+                        <S.MethodItem
+                            itemName={'DEPOSIT'}
+                            content={'무통장 입금'}
+                            onClick={handlePayment}
+                        />
+                        <S.MethodItem
+                            itemName={'PHONE_PAYMENT'}
+                            content={'휴대폰 결제'}
+                            onClick={handlePayment}
+                        />
+                        <S.MethodItem
+                            itemName={'NAVERPAY'}
+                            content={'네이버페이'}
+                            onClick={handlePayment}
+                        />
+                        <S.MethodItem
+                            itemName={'KAKAOPAY'}
+                            content={'카카오페이'}
+                            onClick={handlePayment}
+                        />
                     </S.MethodUl>
-                    <S.BoldHr/>
+                    <S.BoldHr />
                 </S.MethodDiv>
 
                 <S.FinalDiv>
                     <S.InfoTitle>최종결제 정보</S.InfoTitle>
                     <S.FinalContentDiv>
                         <S.FinalPriceDiv>
-                            <S.FinalInfo title={'상품금액'} price={total_product_price}/>
-                            <S.FinalInfo title={'할인금액'} price={'0'}/>
-                            <S.FinalInfo title={'배송비'} price={total_shipping_fee}/>
-                            <S.Hr/>
-                            <S.FinalPriceInfo title={'결제금액'} price={total_price}/>
+                            <S.FinalInfo
+                                title={'상품금액'}
+                                price={total_product_price}
+                            />
+                            <S.FinalInfo title={'할인금액'} price={'0'} />
+                            <S.FinalInfo
+                                title={'배송비'}
+                                price={total_shipping_fee}
+                            />
+                            <S.Hr />
+                            <S.FinalPriceInfo
+                                title={'결제금액'}
+                                price={total_price}
+                            />
                         </S.FinalPriceDiv>
                         <S.AgreementDiv>
-                            <CheckBox isCheck={isCheckAgree} setIsCheck={setIsCheckAgree}/>
-                            <S.AgreementSpan>주문 내용을 확인하였으며, 정보 제공 등에 동의합니다.</S.AgreementSpan>
-                            { isAllInputsValid() ?
-                                <S.PaymentBtn type={'active'} onClick={postOrderData}>결제하기</S.PaymentBtn>
-                                :
-                                <S.PaymentBtn type={'disabled'} disabled>결제하기</S.PaymentBtn>
-                            }
+                            <CheckBox
+                                isCheck={isCheckAgree}
+                                setIsCheck={setIsCheckAgree}
+                            />
+                            <S.AgreementSpan>
+                                주문 내용을 확인하였으며, 정보 제공 등에
+                                동의합니다.
+                            </S.AgreementSpan>
+                            {isAllInputsValid() ? (
+                                <S.PaymentBtn
+                                    type={'active'}
+                                    onClick={postOrderData}
+                                >
+                                    결제하기
+                                </S.PaymentBtn>
+                            ) : (
+                                <S.PaymentBtn type={'disabled'} disabled>
+                                    결제하기
+                                </S.PaymentBtn>
+                            )}
                         </S.AgreementDiv>
                     </S.FinalContentDiv>
                 </S.FinalDiv>
--- src/pages/buyer/PaymentPage/_style.jsx
+++ src/pages/buyer/PaymentPage/_style.jsx
@@ -8,7 +8,7 @@ export const PaymentTitleDiv = styled.div`
     max-width: 1280px;
     margin: 0 auto 16px;
     text-align: center;
-`
+`;
 
 export const PaymentHeader = styled.h1`
     display: inline-block;
@@ -20,7 +20,7 @@ export const PaymentHeader = styled.h1`
     @media screen and (max-width: 768px) {
         font-size: 24px;
     }
-`
+`;
 
 export const MenuUl = styled.ul`
     display: flex;
@@ -32,7 +32,7 @@ export const MenuUl = styled.ul`
     font-size: 17px;
     line-height: 22.54px;
     background-color: var(--menu);
-`
+`;
 
 export const ItemUl = styled.ul`
     width: 100%;
@@ -46,7 +46,7 @@ export const ItemUl = styled.ul`
     @media screen and (max-width: 768px) {
         width: 450px;
     }
-`
+`;
 
 export const TotalPriceDiv = styled.div`
     display: flex;
@@ -55,7 +55,7 @@ export const TotalPriceDiv = styled.div`
     margin: 0 auto;
     padding-top: 14px;
     align-items: center;
-    
+
     @media screen and (max-width: 1280px) {
         width: 900px;
     }
@@ -63,18 +63,18 @@ export const TotalPriceDiv = styled.div`
     @media screen and (max-width: 768px) {
         width: 450px;
     }
-`
+`;
 
 export const TotalSpan = styled.span`
     margin-right: 10px;
     font-weight: var(--semi-bold);
     font-size: 18px;
     line-height: 22.54px;
-`
+`;
 
 export const InfoInputDiv = styled.div`
     width: 1280px;
-    margin: 96px auto 70px;    
+    margin: 96px auto 70px;
 
     @media screen and (max-width: 1280px) {
         width: 900px;
@@ -83,21 +83,21 @@ export const InfoInputDiv = styled.div`
     @media screen and (max-width: 768px) {
         width: 450px;
     }
-`
+`;
 
 export const InfoTitle = styled.h2`
     margin-bottom: 18px;
     font-weight: var(--semi-bold);
     font-size: 24px;
     line-height: 30.05px;
-`
+`;
 
 export const InfoSubTitle = styled.h3`
     margin: 40px 0 8px;
     font-weight: var(--semi-bold);
     font-size: 18px;
     line-height: 22.54px;
-`
+`;
 
 export const InputTitle = styled.p`
     display: inline-block;
@@ -110,23 +110,23 @@ export const InputTitle = styled.p`
     @media screen and (max-width: 768px) {
         width: 120px;
     }
-`
+`;
 
 export const AddressTitle = styled(InputTitle)`
     margin-bottom: 114px;
-`
+`;
 
 export const AddressDiv = styled.div`
     display: flex;
     height: 152px;
-`
+`;
 
 // <hr/>
 export const Hr = styled.div`
     width: 100%;
     height: 1px;
     background-color: var(--disabled);
-`
+`;
 
 export const BoldHr = styled(Hr)`
     height: 2px;
@@ -134,7 +134,7 @@ export const BoldHr = styled(Hr)`
     @media screen and (max-width: 768px) {
         max-width: 500px;
     }
-`
+`;
 
 // input
 export const PaymentInput = styled.input`
@@ -150,7 +150,7 @@ export const PaymentInput = styled.input`
     @media screen and (max-width: 768px) {
         max-width: 234px;
     }
-`
+`;
 
 export const MobileInput1 = styled(PaymentInput)`
     width: 80px;
@@ -162,19 +162,19 @@ export const MobileInput1 = styled(PaymentInput)`
     @media screen and (max-width: 768px) {
         width: 60px;
     }
-`
+`;
 export const MobileInput = styled(MobileInput1)`
     width: 100px;
     @media screen and (max-width: 768px) {
         width: 80px;
     }
-`
+`;
 
 export const EmailInput = styled(PaymentInput)`
     &:invalid {
         border: 1px solid var(--red);
     }
-`
+`;
 
 export const PostNumInput = styled(PaymentInput)`
     width: 170px;
@@ -182,17 +182,17 @@ export const PostNumInput = styled(PaymentInput)`
     @media screen and (max-width: 768px) {
         width: 70px;
     }
-`
+`;
 
 export const AddressInput = styled(PaymentInput)`
     display: block;
     width: 800px;
     margin-top: 0;
-`
+`;
 
 export const ShippingMsgInput = styled(PaymentInput)`
     max-width: 800px;
-    
+
     @media screen and (max-width: 1280px) {
         max-width: 730px;
     }
@@ -200,34 +200,32 @@ export const ShippingMsgInput = styled(PaymentInput)`
     @media screen and (max-width: 768px) {
         max-width: 330px;
     }
-`
+`;
 
 // button
 export const PostNumSearchBtn = styled(BtnS)`
     width: 154px;
     margin-left: 10px;
     background-color: var(--main);
-`
+`;
 
 export const PaymentBtn = styled(BtnL)`
     display: block;
     width: 220px;
     margin: 34px auto 0;
     box-sizing: border-box;
-`
+`;
 
 // hyphen
 const StyleHyphen = styled.span`
     margin: 0 10px;
     font-size: 16px;
     line-height: 20.03px;
-`
+`;
 
 export const Hyphen = () => {
-    return (
-        <StyleHyphen>-</StyleHyphen>
-    )
-}
+    return <StyleHyphen>-</StyleHyphen>;
+};
 
 // 결제수단
 export const PaymentDiv = styled.div`
@@ -244,7 +242,7 @@ export const PaymentDiv = styled.div`
         max-width: 450px;
         flex-direction: column;
     }
-`
+`;
 
 export const MethodDiv = styled.div`
     width: 760px;
@@ -253,7 +251,7 @@ export const MethodDiv = styled.div`
     @media screen and (max-width: 1280px) {
         width: 450px;
     }
-`
+`;
 
 export const MethodUl = styled.ul`
     display: flex;
@@ -265,7 +263,7 @@ export const MethodUl = styled.ul`
         flex-direction: column;
         gap: 0;
     }
-`
+`;
 
 const MethodLi = styled.li`
     display: flex;
@@ -279,22 +277,27 @@ const MethodLi = styled.li`
     @media screen and (max-width: 768px) {
         margin: 8px 0;
     }
-`
+`;
 
 export const MethodInput = styled.input`
     width: 20px;
     height: 20px;
     color: 2px solid var(--disabled);
-`
+`;
 
-export const MethodItem = ({itemName,content, onClick}) => {
+export const MethodItem = ({ itemName, content, onClick }) => {
     return (
         <MethodLi onClick={onClick}>
-            <MethodInput type='radio' id={itemName} name='payment_method' value={itemName}/>
+            <MethodInput
+                type="radio"
+                id={itemName}
+                name="payment_method"
+                value={itemName}
+            />
             <label htmlFor={itemName}>{content}</label>
         </MethodLi>
-    )
-}
+    );
+};
 
 // 최종결제 정보
 export const FinalDiv = styled.div`
@@ -306,7 +309,7 @@ export const FinalDiv = styled.div`
         width: 450px;
         margin-top: 70px;
     }
-`
+`;
 
 export const FinalContentDiv = styled.div`
     width: 100%;
@@ -315,11 +318,11 @@ export const FinalContentDiv = styled.div`
     box-sizing: border-box;
     border: 2px solid var(--main);
     border-radius: 10px;
-`
+`;
 
 export const FinalPriceDiv = styled.div`
-    padding: 30px 28px 0 ;
-`
+    padding: 30px 28px 0;
+`;
 
 const FinalInfoDiv = styled.div`
     display: flex;
@@ -327,40 +330,52 @@ const FinalInfoDiv = styled.div`
     height: 23px;
     margin-bottom: 12px;
     align-items: center;
-`
+`;
 
 const FinalInfoP = styled.p`
     font-weight: var(--regular);
     font-size: 16px;
     line-height: 20.03px;
-`
+`;
 
 const FinalPriceInfoDiv = styled(FinalInfoDiv)`
     height: 74px;
     margin-bottom: 0;
-`
+`;
 
-export const FinalInfo = ({title, price}) => {
+export const FinalInfo = ({ title, price }) => {
     return (
         <FinalInfoDiv>
             <FinalInfoP>{`- ${title}`}</FinalInfoP>
             <div>
-                <Price price={price} numsize={18} margin={4} txtsize={14} color={'--black'}/>
+                <Price
+                    price={price}
+                    numsize={18}
+                    margin={4}
+                    txtsize={14}
+                    color={'--black'}
+                />
             </div>
         </FinalInfoDiv>
-    )
-}
+    );
+};
 
-export const FinalPriceInfo = ({title, price}) => {
+export const FinalPriceInfo = ({ title, price }) => {
     return (
         <FinalPriceInfoDiv>
             <FinalInfoP>{`- ${title}`}</FinalInfoP>
             <div>
-                <Price price={price} numsize={24} margin={0} txtsize={24} color={'--main'}/>
+                <Price
+                    price={price}
+                    numsize={24}
+                    margin={0}
+                    txtsize={24}
+                    color={'--main'}
+                />
             </div>
         </FinalPriceInfoDiv>
-    )
-}
+    );
+};
 
 export const AgreementDiv = styled.div`
     padding: 30px 30px 32px;
@@ -368,13 +383,13 @@ export const AgreementDiv = styled.div`
     border-bottom-left-radius: 10px;
     border-bottom-right-radius: 10px;
     background-color: var(--menu);
-`
+`;
 
 export const AgreementSpan = styled.span`
     font-weight: var(--regular);
     font-size: 16px;
     line-height: 20.03px;
-`
+`;
 
 export const PostCodeContent = styled.div`
     display: inline-block;
@@ -384,7 +399,7 @@ export const PostCodeContent = styled.div`
     border: 1px solid var(--disabled);
     transform: translate(-50%, -50%);
     background-color: var(--white);
-`
+`;
 
 export const PostCodeTitle = styled.div`
     width: 100%;
@@ -392,7 +407,7 @@ export const PostCodeTitle = styled.div`
     border-bottom: 1px solid var(--disabled);
     line-height: 50px;
     text-align: center;
-`
+`;
 
 export const DeleteBtn = styled.button`
     position: absolute;
@@ -401,7 +416,7 @@ export const DeleteBtn = styled.button`
     width: 22px;
     height: 22px;
     background: url(${DelIcon}) no-repeat;
-`
+`;
 
 export const ModalBg = styled.section`
     position: fixed;
@@ -412,5 +427,6 @@ export const ModalBg = styled.section`
     z-index: 150;
     opacity: 1;
     background-color: rgba(0, 0, 0, 0.1);
-    visibility: ${(props) => props.isOpenPostCode === false ? 'hidden' : 'visible'};
-`   
+    visibility: ${(props) =>
+        props.isOpenPostCode === false ? 'hidden' : 'visible'};
+`;
