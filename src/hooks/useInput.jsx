257fbb9ed4da446112f58358e3acd65a0fea7826
“nimoseel” <fromsomini@gmail.com>
2024-01-15 16:12:42 +0900

    Fix : 디바운싱 적용 - 유효성 검사 호출 빈도 줄임

--- src/hooks/useInput.jsx
+++ src/hooks/useInput.jsx
@@ -1,23 +1,35 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const useInput = (initialValue, validator = null, inputName) => {
     const [value, setValue] = useState(initialValue);
     const [error, setError] = useState('');
     const [isValid, setIsValid] = useState(true);
 
+    const debounceTimeout = useRef(null);
+
     const handleChange = (e) => {
         setValue(e.target.value);
     };
 
-    const handleKeyUp = validator
-        ? (e) => {
-              try {
-                  setError(validator(e.target.value));
-              } catch (error) {
-                  setError(error.message);
-              }
-          }
-        : null;
+    useEffect(() => {
+        if (debounceTimeout.current) {
+            clearTimeout(debounceTimeout.current);
+        }
+
+        if (validator && value !== '') {
+            debounceTimeout.current = setTimeout(() => {
+                try {
+                    setError(validator(value));
+                } catch (error) {
+                    setError(error.message);
+                }
+            }, 300);
+        }
+
+        return () => {
+            clearTimeout(debounceTimeout.current);
+        };
+    }, [value, validator]);
 
     return {
         value,
@@ -26,7 +38,6 @@ const useInput = (initialValue, validator = null, inputName) => {
         isValid,
         setIsValid,
         onChange: handleChange,
-        onKeyUp: handleKeyUp,
         name: inputName,
         id: inputName,
     };
